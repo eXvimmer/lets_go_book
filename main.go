2ccbe57acc4cf0d5aaacccfd0d9740599ec95194
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-09-12 16:04:38 +0430

    Create a ServeMux (a router, an HTTP request multiplexer), and start the server.

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox")) // typecast string to []byte
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// NOTE: servemux in Go terminology = router
+	mux := http.NewServeMux() // a new ServeMux: an HTTP request multiplexer
+	mux.HandleFunc("/", home)
+
+	// TODO: make a variable for PORT
+	log.Println("🚀 Starting server on :4000")
+
+	// NOTE: the TCP network address format: host:port
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
