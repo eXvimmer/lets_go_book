ddff980cb8e749a4b981606409a3c7d76d57cdfb
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-09-12 16:12:19 +0430

    Add new routes to view and create a snippet.

--- main.go
+++ main.go
@@ -9,15 +9,24 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox")) // typecast string to []byte
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("View a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func main() {
 	// NOTE: servemux in Go terminology = router
 	mux := http.NewServeMux() // a new ServeMux: an HTTP request multiplexer
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// TODO: make a variable for PORT
 	log.Println("🚀 Starting server on :4000")
 
-	// NOTE: the TCP network address format: host:port
 	err := http.ListenAndServe(":4000", mux)
 	if err != nil {
 		log.Fatal(err)
