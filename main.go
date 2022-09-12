b9a2023a8d3a1c2205d55de1b883ec1e12cbe11d
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-09-12 16:23:37 +0430

    Fix the root URL pattern to not be a catch all.

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Snippetbox")) // typecast string to []byte
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello from Snippetbox"))
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
@@ -20,7 +25,13 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// NOTE: servemux in Go terminology = router
 	mux := http.NewServeMux() // a new ServeMux: an HTTP request multiplexer
+
+	// NOTE: subtree path (ends with a trailing / e.g. /** or /static/** ->
+	// NOTE: acts like a catch-all)
 	mux.HandleFunc("/", home)
+
+	// NOTE: fixed paths (not ending with /): request URL path should exactly
+	// NOTE: match the fixed path
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
