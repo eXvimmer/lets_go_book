13beacbed644330c6dbef4e45f4435daa90ca6b6
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-09-13 21:33:51 +0430

    Configure settings to use a command line flag for address.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	// NOTE: you need to Parse flags before using them, otherwise defaults will be used
+	flag.Parse()
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux := http.NewServeMux()
 
+	// TODO: https://www.alexedwards.net/blog/disable-http-fileserver-directory-listings
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("🚀 Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("🚀 Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
