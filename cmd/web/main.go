abe9e296eb3c674dc7b37a412e4356a7f229519d
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-09-18 00:54:47 +0430

    Add leveled logging to the app.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,16 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	// NOTE: you need to Parse flags before using them, otherwise defaults will be used
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
@@ -21,9 +25,9 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("🚀 Starting server on %s", *addr)
+	infoLog.Printf("🚀 Starting server on %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
-		log.Fatal(err)
+		errorLog.Fatal(err)
 	}
 }
