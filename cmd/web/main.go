fc0740415d09577f72c90d6e7fbe44b6f79ccff5
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-09-22 00:22:00 +0330

    Create a database connection pool.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 type application struct {
@@ -13,11 +17,35 @@ type application struct {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	/* NOTE:
+	 * you should create a .env file at the root of your project and add
+	 * MYSQL_USERNAME & MYSQL_PASSWORD to it.
+	 */
+	dbUsername := os.Getenv("MYSQL_USERNAME")
+	dbPassword := os.Getenv("MYSQL_PASSWORD")
+	dsn := flag.String(
+		"dsn",
+		dbUsername+":"+dbPassword+"@/snippetbox?parseTime=true",
+		"MySQL data source name",
+	)
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -31,8 +59,23 @@ func main() {
 	}
 
 	infoLog.Printf("🚀 Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		errorLog.Fatal(err)
 	}
+}
+
+// The openDB() function wraps sql.Open() and returns a sql.DB connection pool
+// for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/exvimmer/lets_go/snippetbox
 
 go 1.19
 
-require github.com/go-sql-driver/mysql v1.6.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
