ea93a78ded200b68731bafc084e7e32c96707813
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-10-01 13:17:23 +0330

    Cache templates and create a helper method to render pages.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,10 +1,42 @@
 package main
 
-import "github.com/exvimmer/lets_go/snippetbox/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/exvimmer/lets_go/snippetbox/internal/models"
+)
 
 // The holding struct for any dynamic data that we want to pass to our HTML
 // templates.
 type templateData struct {
 	Snippets []*models.Snippet
 	Snippet  *models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page) // e.g. home.tmpl.html
+		files := []string{
+			"./ui/html/base.tmpl.html",
+			"./ui/html/partials/nav.tmpl.html",
+			page,
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
