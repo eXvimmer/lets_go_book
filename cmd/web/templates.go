51432eee33a23324bfe41cdabe4dcb1079aa3ea3
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-10-02 01:58:36 +0330

    Create the humanDate custom template function.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/exvimmer/lets_go/snippetbox/internal/models"
 )
@@ -15,6 +16,16 @@ type templateData struct {
 	Snippet     *models.Snippet
 }
 
+// returns a nicely formatted string representation of a time.Time object
+func humanDate(t time.Time) string {
+	// NOTE: the structure of layout string is very important; check the doc
+	return t.Format("06 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
@@ -25,8 +36,15 @@ func newTemplateCache() (map[string]*template.Template, error) {
 
 	for _, page := range pages {
 		name := filepath.Base(page) // e.g. home.tmpl.html
-
-		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		/*  NOTE:
+		 * The template.FuncMap must be registered with the template set before you
+		 * call the ParseFiles() method. This means we have to use template.New()
+		 * to create an empty template set, use the Funcs() method to register the
+		 * template.FuncMap, and then parse the file as normal.
+		 */
+		ts, err := template.New(name).Funcs(functions).ParseFiles(
+			"./ui/html/base.tmpl.html",
+		)
 		if err != nil {
 			return nil, err
 		}
@@ -37,7 +55,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 			return nil, err
 		}
 
-		ts, err = ts.ParseGlob(page)
+		ts, err = ts.ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
