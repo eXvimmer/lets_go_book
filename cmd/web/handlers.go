5978f5b77094ff0525ee22c1dd6265c3c8b8b6a3
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-11-19 20:34:51 +0330

    Validate form data.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/exvimmer/lets_go/snippetbox/internal/models"
 	"github.com/julienschmidt/httprouter"
@@ -42,7 +44,6 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 	data := app.newTemplateData(r)
 	data.Snippet = s
-	fmt.Println(s)
 
 	app.render(w, http.StatusOK, "view.tmpl.html", data)
 }
@@ -59,11 +60,40 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	// NOTE: r.PostForm.Get only returns the first value for a specific form
+	// field. If you want to use it with form fields which potentially send
+	// multiple values, e.g. checkboxes, you can loop over the underlying map to
+	// access them like so:
+	// for i, item := range r.PostForm["items"] {...}
+
 	title := r.PostForm.Get("title")
 	content := r.PostForm.Get("content")
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// code patterns for validating different types of inputes:
+	// https://www.alexedwards.net/blog/validation-snippets-for-go
+	fieldErrors := make(map[string]string)
+
+	if strings.TrimSpace(title) == "" {
+		fieldErrors["title"] = "this field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		fieldErrors["title"] = "this field cannot be more than 100 characters long"
+	}
+
+	if strings.TrimSpace(content) == "" {
+		fieldErrors["content"] = "this field cannot be blank"
+	}
+
+	if expires != 1 && expires != 7 && expires != 365 {
+		fieldErrors["expires"] = "This field must equal 1, 7 or 365"
+	}
+
+	if len(fieldErrors) > 0 {
+		fmt.Fprint(w, fieldErrors)
 		return
 	}
 
