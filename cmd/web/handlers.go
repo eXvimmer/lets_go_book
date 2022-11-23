dd6ae758d4637a1ca78a9914e966c9ec8ab8912f
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-11-23 18:49:08 +0330

    Log out a user.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -211,7 +211,13 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 }
 
-// TODO: complete these handlers
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user...")
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+	app.sessionManager.Put(r.Context(), "flash", "you've been logged out successfully")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
