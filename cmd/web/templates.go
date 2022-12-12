51fe1e57d3a4a418f95e832469b7842f122e66d4
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-12-12 10:31:15 +0330

    Embed html templates.

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
 
 	"github.com/exvimmer/lets_go/snippetbox/internal/models"
+	"github.com/exvimmer/lets_go/snippetbox/ui"
 )
 
 // The holding struct for any dynamic data that we want to pass to our HTML
@@ -22,7 +24,6 @@ type templateData struct {
 
 // returns a nicely formatted string representation of a time.Time object
 func humanDate(t time.Time) string {
-	// NOTE: the structure of layout string is very important; check the doc
 	return t.Format("02 Jan 2006 at 15:04")
 }
 
@@ -33,33 +34,20 @@ var functions = template.FuncMap{
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl.html")
 	if err != nil {
 		return nil, err
 	}
 
 	for _, page := range pages {
 		name := filepath.Base(page) // e.g. home.tmpl.html
-		/*  NOTE:
-		 * The template.FuncMap must be registered with the template set before you
-		 * call the ParseFiles() method. This means we have to use template.New()
-		 * to create an empty template set, use the Funcs() method to register the
-		 * template.FuncMap, and then parse the file as normal.
-		 */
-		ts, err := template.New(name).Funcs(functions).ParseFiles(
-			"./ui/html/base.tmpl.html",
-		)
-		if err != nil {
-			return nil, err
+		patterns := []string{
+			"html/base.tmpl.html",
+			"html/partials/*.tmpl.html",
+			page,
 		}
 
-		// add any partials
-		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
