9b50dc18724c84bab9fd3eec74839014e8718c02
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-12-12 11:28:58 +0330

    Change the PermittedInt validator to be generic PermittedValue function.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -89,7 +89,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	form.CheckField(
 		validator.NotBlank(form.Content), "content", "this field cannot be blank")
 	form.CheckField(
-		validator.PermittedInt(form.Expires, 1, 7, 365),
+		validator.PermittedValue(form.Expires, 1, 7, 365),
 		"expires", "this field must equal 1, 7, or 365")
 
 	if !form.Valid() {
