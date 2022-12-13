d64afaa3d32ea579eaa48663bb58d3f4578b1e08
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-12-13 08:21:15 +0330

    Add an end-to-end testing for ping handler function using httptest.NewTLSServer (or httptest.Server in case of http).

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,18 +12,16 @@ import (
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder() // the responseRecorder
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	app := &application{
+		errorLog: log.New(io.Discard, "", 0),
+		infoLog:  log.New(io.Discard, "", 0),
+	}
+	ts := httptest.NewTLSServer(app.routes()) // use httptest.NewServer for http
+	defer ts.Close()
+	rs, err := ts.Client().Get(ts.URL + "/ping")
 	if err != nil {
-		// NOTE: Typically you should call t.Fatal() in situations where it doesn't
-		// make sense to continue the current test — such as an error during a
-		// setup step, or where an unexpected error from a Go standard library
-		// function means you can't proceed with the test.
 		t.Fatal(err)
 	}
-
-	ping(rr, r)
-	rs := rr.Result()
 	defer rs.Body.Close()
 	assert.Equal(t, rs.StatusCode, http.StatusOK)
 
