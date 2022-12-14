9b04a059ea286f0c0aa41ff716872845892c48de
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-12-14 20:52:51 +0330

    Add an about page to the application.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -35,6 +35,11 @@ func ping(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func (app *application) about(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+	app.render(w, http.StatusOK, "about.tmpl.html", data)
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	snippets, err := app.snippets.Latest()
 	if err != nil {
